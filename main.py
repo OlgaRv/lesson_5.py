5c148650e0f8e1838f50b1981489a871ee9b9bea
OlgaRv <olgavr2959@gmail.com>
2024-03-28 22:08:20 +0300

    New code ManagerTask

---
+++ main.py
@@ -0,0 +1,40 @@
+class Task:
+    def __init__(self, description, deadline):
+        self.description = description
+        self.deadline = deadline
+        self.status = False  # по умолчанию задача не выполнена
+
+
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, description, deadline):
+        task = Task(description, deadline)
+        self.tasks.append(task)
+        print(f"Задача '{description}' добавлена")
+
+    def mark_task_as_done(self, description):
+        for task in self.tasks:
+            if task.description == description:
+                task.status = True
+                print(f"Задача '{description}' выполнена")
+                return
+        print(f"Задача '{description}' не найдена")
+
+    def get_current_tasks(self):
+        current_tasks = [task for task in self.tasks if not task.status]
+        if current_tasks:
+            print("Текущие задачи:")
+            for task in current_tasks:
+                print(
+                    f"Описание: {task.description}, Срок: {task.deadline}, Статус: {'выполнено' if task.status else 'не выполнено'}")
+        else:
+            print("Нет текущих задач")
+
+
+task_manager = TaskManager()
+task_manager.add_task("Сделать уроки", "01.10.2022")
+task_manager.add_task("Сходить в магазин", "05.10.2022")
+task_manager.mark_task_as_done("Сделать уроки")
+task_manager.get_current_tasks()
--- manager_task.py
+++ manager_task.py
@@ -1,7 +1,46 @@
 # Менеджер задач
-#
-# Задача: Создай класс Task, который позволяет управлять задачами (делами).
+# Создай класс Task, который позволяет управлять задачами (делами).
 # У задачи должны быть атрибуты: описание задачи, срок выполнения и статус (выполнено/не выполнено).
 # Реализуй функцию для добавления задач, отметки выполненных задач и вывода списка текущих (не выполненных) задач.
 
 class Task():
+    def __init__(self, description, deadline):
+        self.description = description
+        self.deadline = deadline
+        self.status = False
+class TaskManager:
+    def __init__(self):
+        self.tasks = []
+
+    def add_task(self, description, deadline):
+        task = Task(description, deadline)
+        self.tasks.append(task)
+        print(f"Задача '{description}' добавлена")
+
+    def mark_task_as_done(self, description):
+        for task in self.tasks:
+            if task.description == description:
+                task.status = True
+                print(f"Задача '{description}' выполнена")
+                return
+        print(f"Задача '{description}' не найдена")
+
+    def get_current_tasks(self):
+        current_tasks = []
+        for task in self.tasks:
+            if task.status == False:
+                current_tasks.append(task)
+        if current_tasks:
+            print("Текущие задачи:")
+            for task in current_tasks:
+                print(f"Описание: {task.description}, Срок: {task.deadline}, Статус: не выполнено")
+        else:
+            print("Нет текущих задач")
+
+
+task_manager = TaskManager()
+task_manager.add_task("Сходить в магазин", "29.03.2024")
+task_manager.add_task("Погулять с собакой", "29.03.2024")
+task_manager.add_task("Вынести мусор", "30.03.2024")
+task_manager.mark_task_as_done("Погулять с собакой")
+task_manager.get_current_tasks()
